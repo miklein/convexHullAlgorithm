67c528797cda2bd4c30313819e0dcc75b26b521c
miklein <michael.klein08@gmail.com>
2013-11-18 10:52:07 +0100

    add comments

--- src/de/kellertobias/hs/algolab/convexhull/algorithm/ChanAlgorithm.java
+++ src/de/kellertobias/hs/algolab/convexhull/algorithm/ChanAlgorithm.java
@@ -17,23 +17,21 @@ public class ChanAlgorithm implements Algorithm {
 		if (dataset.size() < 3) {
 			// do something..
 		}
-		
-		//int i++;
-		//int m = Math.min(Math.pow(2, Math.pow(2, i)), dataset.size());
-		
+				
 		int i = 0;
 		int m = 0;
-		Algorithm algo = new MonotoneChainAlgorithm();
+		Algorithm monotoneChainAlgo = new MonotoneChainAlgorithm();
 		
 		do {
-			m = (int) Math.min(Math.pow(2, Math.pow(2, i)), dataset.size());
+			m = (int) Math.min(Math.pow(2, Math.pow(2, i)), dataset.size());		// m = min{2^2^i, n}
 			
-			List<List<Point>> subsets = this.createSubsets(dataset, m);
+						
+			List<List<Point>> subsets = createSubsets(dataset, m);
 			List<List<Point>> convexHulls = new ArrayList<List<Point>>();
 			
 						
 			for (List<Point> subset : subsets) {
-				convexHulls.add(algo.calculate(subset));
+				convexHulls.add(monotoneChainAlgo.calculate(subset));
 			}
 			
 			
@@ -43,31 +41,34 @@ public class ChanAlgorithm implements Algorithm {
 			// javis march
 			List<Point> output = new ArrayList<Point>();
 			output.add(farthesLeftPoint);
-			int hullIndex = 1;
+			int hullIndex = 1;			//Begin with second hull point
+			// Roundlength depend of the choosen size of m
 			for (int j=0; j < m; j++) {
 				
 				// calculate angle between last output point and the stepIndex-Point of the subset
 				Point startpoint = output.get(output.size()-1);
 				
-				Point tempPoint = null;
-				
-				for (int k=0; k < convexHulls.size(); k++) {
-					int safeHullIndex = hullIndex % convexHulls.get(k).size();
-					System.out.println("k="+k+ " hullIndex="+hullIndex+"safeHullIndex="+safeHullIndex);
-					System.out.println("hull:"+convexHulls.get(k));
-					if (tempPoint == null || convexHulls.get(k).get(safeHullIndex).isAboveLine(startpoint, tempPoint) == 1) {
-						tempPoint = convexHulls.get(k).get(safeHullIndex);
-						System.out.println("neues temp="+tempPoint);
+				Point newStartPoint = null;					
+						
+				for (int currentHull = 0; currentHull < convexHulls.size(); currentHull++) {
+					int safeHullIndex = hullIndex % convexHulls.get(currentHull).size();
+					System.out.println("currentHull="+currentHull + " hullIndex="+hullIndex+" safeHullIndex="+safeHullIndex);
+					System.out.println("hull:"+convexHulls.get(currentHull));
+					//If current point from convexHullPart is left from line startpoint -> 
+					if (newStartPoint == null || convexHulls.get(currentHull).get(safeHullIndex).isAboveLine(startpoint, newStartPoint) == 1) {
+						newStartPoint = convexHulls.get(currentHull).get(safeHullIndex);
+						System.out.println("new startpoint="+newStartPoint);
 					}
 				}
 				
-				if (tempPoint == farthesLeftPoint) {
+				// Terminate if back at farthest left point -> startpoint of the hull
+				if (newStartPoint == farthesLeftPoint) {
 					System.out.println("FINALE Lösung Runde "+i+" => "+ output);
 					return output;
 				}
 
-				if (tempPoint != null) {
-					output.add(tempPoint);
+				if (newStartPoint != null) {
+					output.add(newStartPoint);
 				}
 				hullIndex++;
 			}
